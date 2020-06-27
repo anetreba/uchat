4e92384070ce8dfcc82460d329db42a60fe066e4
Oleh Voitenko <voytenko.ov@gmail.com>
2020-06-27 03:28:53 +0300

    db

--- server/src/controllers/mx_signup.c
+++ server/src/controllers/mx_signup.c
@@ -1,27 +1,24 @@
 #include "header.h"
 
 static int callback_signup(void *data, int argc, char **argv, char **ColName) {
-    t_data udata;
+    t_data *udata = NULL;
     data = 0;
 
-    udata.login = argv[0];
-    udata.argc = argc;
-    udata.colname = ColName;
+    udata->login = argv[0];
+    udata->argc = argc;
+    udata->colname = ColName;
     return 0;
 }
 
 int mx_contr_signup(const char *log_in, const char *pass, const char *nickname) {
     char *vals;
-    t_data data;
-    char check_reg;
+    char *data = NULL;
 
-    asprintf(&vals, "Users WHERE login = '%s'", nickname);
-    check_reg = mx_model_select("login", vals, callback_signup, &data);
-    if(data.login) {
-        asprintf(&vals, "'%s','%s','%s'", log_in, pass, nickname);
-        mx_model_insert("Users", "login, pass, nick", vals);
-        return 0;
-    }
-    else
-        return 1;
+    asprintf(&vals, "Users WHERE login = '%s'", log_in);
+    mx_model_select("login", vals, callback_signup, &data);
+
+    asprintf(&vals, "'%s','%s','%s'", log_in, pass, nickname);
+    mx_model_insert("Users", "login, pass, nick", vals);
+            return 0;
+        //return 1;
 }
