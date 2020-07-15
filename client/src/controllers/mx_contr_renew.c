924a70eb6c25d7c28eca362419ef6e798554d8d9
Oleh Voitenko <voytenko.ov@gmail.com>
2020-07-15 22:52:37 +0300

    signin signup

--- client/src/controllers/mx_contr_renew.c
+++ client/src/controllers/mx_contr_renew.c
@@ -1,11 +1,42 @@
 #include "header.h"
 
+static void write_data_to_db(t_event *event, json_object *obj) {
+    event->send_message = (t_send_message *)malloc(sizeof(t_send_message));
+    event = NULL;
+    obj = NULL;
+
+//    for (int i = 0; i < 3; i++) {
+//        struct json_object *pos;
+//
+//        print_json_object(obj, "Before the glitch in the matrix");
+//
+//        pos = json_object_array_get_idx(obj, i);
+//
+//        // json_object_array_put_idx decrement the refcount if you replace an
+//        // element, but we don't whant that.
+//        json_object_array_put_idx(obj, i, json_object_get(pos));
+//
+//        print_json_object(obj, "After the glitch in the matrix");
+//
+//
+//
+//    }
+
+
+
+
+
+}
+
 
 void mx_json_read(t_event *event) {
     int n = 0;
     char buf;
     char *str = mx_strnew(0);
     struct json_object *obj = json_object_new_object();
+
+    event->data = (t_data *)malloc(sizeof(t_data));
+    memset(event->data, 0, sizeof(t_data));
 
     while ((n = read(event->network_socket, &buf, 1)) > 0) {
         str = mx_parse_str(str, buf);
@@ -14,19 +45,12 @@ void mx_json_read(t_event *event) {
         }
     }
     parse_json((const char *)str, &obj);
-
-    printf("JOBJ = %s\n", json_object_to_json_string(obj));
+    write_data_to_db(event, obj);
 }
 
 void mx_contr_renew(t_event *event) {
     struct json_object *jobj = json_object_new_object();
-    //event->data = (t_data *)malloc(sizeof(t_data));
-    //memset(event->data, 0, sizeof(t_data));
 
-    printf("================================================\n");
-    printf("ID %d\n", event->data->id);
-    printf("TOKEN %s\n", event->data->auth_token);
-    printf("================================================\n");
     if (event->data) {
         json_object_object_add(jobj, "event", json_object_new_string("renew"));
         json_object_object_add(jobj, "id", json_object_new_int(event->data->id));
@@ -40,14 +64,9 @@ void mx_contr_renew(t_event *event) {
         printf("JSON  == %s\n", jstr);
 
         send(event->network_socket, jstr, strlen(jstr), 0);
-        //mx_strdel(&jstr);
+        mx_strdel(&jstr);
         mx_json_read(event);
     }
-    else {
-        printf("empty");
-    }
-
-
-
-    //write_data(jobj);
+    else
+        printf("WAT`S WRONG");
 }
