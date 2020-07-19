e7efd96de06164ea91619d12461e5fca029c7eb0
Oleh Voitenko <voytenko.ov@gmail.com>
2020-07-19 21:37:44 +0300

    Add files via upload

--- client/src/controllers/mx_contr_renew.c
+++ client/src/controllers/mx_contr_renew.c
@@ -1,5 +1,18 @@
 #include "header.h"
 
+//void mx_print_list(t_list *lst) {
+//    while(lst) {
+//        printf("********************************************************\n");
+//        printf("MESSAGE = %s\n",((t_renew *)(lst->data))->message);
+//        printf("ROOM ID = %d\n",((t_renew *)(lst->data))->room_id);
+//        printf("ROOM NAME = %s\n",((t_renew *)(lst->data))->name_room);
+//        printf("SENDER ID = %d\n",((t_renew *)(lst->data))->sender_id);
+//        printf("DATA SEND = %d\n",((t_renew *)(lst->data))->date_send);
+//        printf("REC STATUS = %d\n",((t_renew *)(lst->data))->recieve_status);
+//        lst = lst->next;
+//        printf("********************************************************\n");
+//    }
+//}
 static int callback_check_msg(void *data, int argc, char **argv, char **ColName) {
     ColName = NULL;
 
@@ -49,20 +62,29 @@ static void write_data_to_db(json_object *obj) {
     }
 }
 
-void mx_contr_renew(json_object *jobj) {
-    t_renew *udata = (t_renew *)malloc(sizeof(t_renew));
-    memset(udata, 0, sizeof(t_renew));
-    t_list *lst = (t_list *)malloc(sizeof(t_list));
 
-    lst =  mx_create_node(udata);
-    json_parse(jobj, lst);
-    mx_pop_front(&lst);
-    if (lst) {
-        while (lst) {
-            if (mx_check_msgs(lst) != 1) {
-                write_data_to_db(jobj);
-            }
-            lst = lst->next;
-        }
+static void mx_json_renew(json_object *obj) {
+    char *str = (char *)json_object_to_json_string(obj);
+
+    parse_json((const char *)str, &obj);
+    write_data_to_db(obj);
+}
+
+void mx_contr_renew(t_event *event, json_object *jobj) {
+    struct json_object *obj = json_object_new_object();
+    char *jstr;
+
+    if (event->data) {
+        json_object_object_add(jobj, "event", json_object_new_string("renew"));
+        json_object_object_add(jobj, "id", json_object_new_int(event->data->id));
+        json_object_object_add(jobj, "auth_token", json_object_new_string(event->data->auth_token));
+        json_object_object_add(jobj, "last_renew", json_object_new_int(0));
+        jstr = (char *)json_object_to_json_string(obj);
+        send(event->network_socket, jstr, strlen(jstr), 0);
+        printf("RENEW_REQ: %s\n", jstr);
+        mx_strdel(&jstr);
+        mx_json_renew(obj);
     }
+    else
+        printf("WAT`S WRONG");
 }
