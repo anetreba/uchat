20ed909b1b40ff9fc9e45099604c21a88182eb62
Oleh Voitenko <voytenko.ov@gmail.com>
2020-07-19 16:09:05 +0300

    Update header.h

--- client/inc/header.h
+++ client/inc/header.h
@@ -62,6 +62,7 @@ typedef struct s_data {
 }               t_data;
 
 typedef struct s_renew {
+    int status;
     int room_id;
     char *name_room;
     char *message;
@@ -107,7 +108,7 @@ typedef struct s_response {
 }               t_response;
 
 
-void mx_valid_event(struct json_object *jobj, int sock);
+void mx_valid_event(struct json_object *jobj, t_event *event);
 //crud
 void mx_model_update(char *table, char *str, char *condition);
 void mx_model_insert (char *table, char *rows, char *vals);
@@ -116,7 +117,6 @@ int mx_model_select(const char *search, char *tables,
                     void *data);
 int mx_init_sqli(char *sql, int (*callback)(void *, int, char **, char **), void *data);
 
-void mx_valid_event(struct json_object *jobj, int sock);
 int mx_init_sqli(char *sql, int (*callback)(void *, int, char **, char **), void *data);
 void mx_server_socket(int port);
 void mx_printerr(char *str);
@@ -124,11 +124,13 @@ char *mx_parse_str(char *jstr, char buf);
 int parse_json(const char *json, json_object **responses);
 t_response *mx_model_logined(t_data *data);
 void json_parse(json_object *jobj, t_list *lst);
-int mx_update_rooms(t_list *lst);
 
-
+void mx_json_read(t_event *event);
 //controllers
 void mx_contr_auth(t_event *event, json_object *jobj);
-void mx_contr_renew(t_event *event);
+void mx_contr_renew(json_object *jobj);
+void mx_contr_update_rooms(json_object *jobj);
+
+void mx_json(t_event *event, char *action);
 
 #endif
