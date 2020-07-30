2b9cf06dc3ac1c76549a42d1a9dedaecd1fee650
Oleh Voitenko <voytenko.ov@gmail.com>
2020-07-30 19:31:43 +0300

    gotovo

---
+++ client/src/db/mx_table_messages.c
@@ -0,0 +1,13 @@
+#include "header.h"
+
+void mx_table_messages(sqlite3 *db, char *sql) {
+    sprintf(sql, "CREATE TABLE IF NOT EXISTS 'Messages' (" \
+        "id             INTEGER PRIMARY KEY AUTOINCREMENT," \
+        "message        TEXT," \
+        "sender_id      INTEGER," \
+        "room_id        INTEGER," \
+        "resieve_status INTEGER," \
+        "date_send      INTEGER," \
+        "type           INTEGER)");
+    mx_create_table(db, sql, mx_callback);
+}
---
+++ client/src/db/mx_table_rooms.c
@@ -0,0 +1,9 @@
+#include "header.h"
+
+void mx_table_rooms(sqlite3 *db, char *sql) {
+    sprintf(sql, "CREATE TABLE IF NOT EXISTS 'Rooms'(" \
+        "id             INTEGER PRIMARY KEY AUTOINCREMENT," \
+        "room_name      VARCHAR(50) NOT NULL, " \
+        "room_id        INTEGER)");
+    mx_create_table(db, sql, mx_callback);
+}
