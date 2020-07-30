7a126f799522265fd5ee7953ab93f7ca9accbc85
anetreba <anetreba@student.ucode.world>
2020-07-30 23:34:12 +0300

    make add room1

--- client/src/mx_add_new_room.c
+++ client/src/mx_add_new_room.c
@@ -5,13 +5,53 @@ void back_to_chat(GtkButton *button, t_event *event) {
     (void)button;
 }
 
+
+void mx_del_show_cont(t_event *event) {
+    t_list *lst = event->for_new_room;
+    t_list *list = event->info_room->cont;
+
+    t_list *prev = list;
+
+    while (list) {
+        while (lst) {
+            if (((t_cont *) (list->data))->id == ((t_cont_for_add_room *)(lst->data))->id)
+                gtk_widget_show(((t_cont_for_add_room *)(lst->data))->cont_btn);
+            lst = lst->next;
+        }
+        lst = event->for_new_room;
+        list = list->next;
+        // free(((t_cont *)(prev->data)));
+        free(prev);
+        prev = list;
+
+    }
+    event->info_room->cont = NULL;
+//    mx_print2(event->info_room->cont);
+}
+
+
+
 void room_confirm_btn(GtkButton *button, t_event *event) {
     event->info_room->room_name = (char *)(gtk_entry_get_text(GTK_ENTRY(event->gtk->add_room_entry_field)));
 
-    mx_json(event, "add_room");
-    gtk_widget_hide(event->gtk->groups_wdw);
-    printf("ROOM_NAME = %s\n", event->info_room->room_name);
+    if (strcmp(event->info_room->room_name, "") != 0)
+        mx_json(event, "add_room");
+
+    mx_del_show_cont(event);
+    gtk_entry_set_text(GTK_ENTRY(event->gtk->add_room_entry_field), "");
+
+//    gtk_widget_hide(event->gtk->groups_wdw);
+
+    //printf("ROOM_NAME = %s\n", event->info_room->room_name);
     (void)button;
+
+//    event->info_room->room_name = (char *)(gtk_entry_get_text(GTK_ENTRY(event->gtk->add_room_entry_field)));
+//
+//    mx_json(event, "add_room");
+//
+//    gtk_widget_hide(event->gtk->groups_wdw);
+//    printf("ROOM_NAME = %s\n", event->info_room->room_name);
+//    (void)button;
 }
 
 void mx_select_cont(GtkButton *button, t_event *event) {
@@ -19,10 +59,10 @@ void mx_select_cont(GtkButton *button, t_event *event) {
 
     t_cont *add_cont = (t_cont *)malloc(sizeof(t_cont));
 
-    add_cont->nick = strdup(cont->nick);
+//    add_cont->nick = strdup(cont->nick);
     add_cont->id = cont->id;
     mx_push_back(&(event->info_room->cont), add_cont);
-    printf("NICK = %s\n", add_cont->nick);
+//    printf("NICK = %s\n", add_cont->nick);
     printf("ID = %d\n", cont->id);
     mx_push_back(&(event->info_room->cont), add_cont);
     gtk_widget_hide(GTK_WIDGET(button));
@@ -79,28 +119,67 @@ void mx_show_groups_wdw(GtkButton *button, t_event *event) {
     (void)button;
 }
 
-void new_room(GtkButton *button, t_event *event) {
+
+void mx_create_new_mess(t_list *lst, t_event *event) {
+    t_mess *mess = (t_mess *)malloc(sizeof(t_mess));
+
+    memset(mess, 0, sizeof(t_mess));
+    mess->sender_nick = event->data->login;
+    mess->message = "CREATE A NEW ROOM!";
+    mess->sender_id = event->data->id;
+    mx_push_back(&lst, mess);
+}
+
+gboolean mx_new_room(void *data) {
+    t_event *event = (t_event *)data;
     t_list_room *room = (t_list_room *)malloc(sizeof(t_list_room));
+    printf("ERROR\n");
 
     memset(room, 0, sizeof(t_list_room));
-    room->room_name = strdup("new_room");
+
+    room->room_name = strdup(event->info_room->room_name);
+    room->room_id = event->info_room->room_id;
+
+    room->list_box = gtk_list_box_new();
+
+    gtk_widget_set_name(room->list_box, "chat_room");
 
     room->row = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
     room->room_btn = gtk_button_new_with_label(room->room_name);
+
+    mx_create_new_mess(room->mess, event);
+
+    mx_push_back(&(event->list_room), room);
+
+    //room->mess = (t_list *)malloc(sizeof(t_list));
+
+//    t_mess *mess = (t_mess *)malloc(sizeof(t_mess));
+
+//    mess->message = "new room create";
+//    mess->sender_id = event->data->id;
+//    mess->sender_nick = event->data->login;
+//
+//    room->mess = mx_create_node(mess);
+    g_object_set_data(G_OBJECT(room->room_btn), "room", room);
 
     gtk_widget_set_hexpand(room->room_btn, TRUE);
     gtk_widget_set_halign(room->room_btn, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(room->room_btn, GTK_ALIGN_CENTER);
     gtk_widget_set_size_request(room->room_btn, 60, 5);
+
+    gtk_widget_set_name(room->room_btn, "room_button");
+
     gtk_container_add(GTK_CONTAINER(room->row), room->room_btn);
 
     gtk_list_box_insert(GTK_LIST_BOX(event->gtk->list_rooms), room->row, -1);
 
+    gtk_container_add(GTK_CONTAINER(event->gtk->viewport), room->list_box);
+    g_signal_connect(room->room_btn, "clicked", G_CALLBACK(mx_select_room), event);
+
     gtk_widget_show(room->row);
     gtk_widget_show(room->room_btn);
 
-    mx_push_back(&(event->list_room), room);
-
-    (void)button;
+    return 0;
 }
+
 
